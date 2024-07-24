55b79950bd6e5399f2b0ee50ca9fb659ef9558e3
nveedee <noel@von-daeniken.ch>
2024-07-24 13:55:35 +0200

    Revert "axa coins api"
    This reverts commit e565b800232b57860da3b71ea77f60790645ce3a.

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -3,34 +3,18 @@ import { Calendar, Modal } from 'antd';
 import moment from 'moment';
 import styles from '../styles/calendar.module.css'; // Importiere das Stylesheet
 
-// Beispiel-Daten für den Kalender mit Ereignissen, die mehrere Tage umfassen
+// Beispiel-Daten für den Kalender
 const events = [
-    { startDate: '2024-07-15', endDate: '2024-07-21', content: 'Da Vinci Projekt', description: 'Projektarbeit für Da Vinci - letzte Woche', color: '#ff9999' },
-    { startDate: '2024-07-22', endDate: '2024-07-28', content: 'Da Vinci Projekt', description: 'Projektarbeit für Da Vinci - diese Woche', color: '#99ff99' },
-    { startDate: '2024-07-29', endDate: '2024-08-18', content: '3 Wochen Urlaub', description: 'Urlaub für 3 Wochen', color: '#9999ff' },
-    { startDate: '2024-08-19', endDate: '2024-08-19', content: 'Da Vinci Vorstellung', description: 'Vorstellung des Da Vinci Projekts', color: '#ffcc99' },
+    { date: '2024-07-22', content: 'Meeting mit dem Team', description: 'Besprechung zur Projektkoordination' },
+    { date: '2024-07-25', content: 'Projekt-Deadline', description: 'Abgabe des Projektberichts' },
+    { date: '2024-07-30', content: 'ddd', description: 'Weitere Details für diesen Tag' },
 ];
-
-
-// Überprüft, ob das gegebene Datum innerhalb des Ereigniszeitraums liegt
-const isDateInRange = (date, startDate, endDate) => {
-    return moment(date).isSameOrAfter(startDate) && moment(date).isSameOrBefore(endDate);
-};
 
 const dateCellRender = (value) => {
     const formattedDate = value.format('YYYY-MM-DD');
-    const event = events.find(event => isDateInRange(formattedDate, event.startDate, event.endDate));
-    return event ? (
-        <div
-            className={styles.event}
-            onClick={() => handleEventClick(event)}
-            style={{ backgroundColor: event.color }} // Setze die Hintergrundfarbe für das Event
-        >
-            {event.content}
-        </div>
-    ) : null;
+    const event = events.find(event => event.date === formattedDate);
+    return event ? <div className={styles.event} onClick={() => handleEventClick(event)}>{event.content}</div> : null;
 };
-
 
 const handleEventClick = (event) => {
     Modal.info({
@@ -46,7 +30,7 @@ const handleEventClick = (event) => {
 
 const MyCalendar = () => {
     return (
-        <div className={styles.calendarCard}>
+        <div>
             <h2>Kalender</h2>
             <Calendar dateCellRender={dateCellRender} />
         </div>
--- src/components/SbbApi.tsx
+++ src/components/SbbApi.tsx
@@ -1,84 +1,16 @@
+'use client';
+
 import { useEffect, useState } from "react";
 import styles from '../styles/sbb.module.css';
 
-// Initialer Token-Wert
-const INITIAL_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsZWFkZXJib2FyZCIsImlhdCI6MTcyMTgwOTExMSwiZXhwIjoxNzIxODk1NTExfQ.Lh-roYWhL-Grz87gCysBSBjNyXp--OfzVpjh-Z47HRU';
-const AUTH_URL = 'http://192.168.100.16:8080/api/auth/authenticate';
-const TOKEN_REFRESH_INTERVAL = 12 * 60 * 60 * 1000; // 12 Stunden in Millisekunden
-
 export default function SbbApi() {
     const [stationboard, setStationboard] = useState([]);
-    const [leaderboard, setLeaderboard] = useState([]);
-    const [showHelloWorld, setShowHelloWorld] = useState(false);
-    const [apiToken, setApiToken] = useState(INITIAL_TOKEN);
 
-    // Fetch für die Stationboard-Daten
     useEffect(() => {
         fetch("https://transport.opendata.ch/v1/stationboard?station=Winterthur&limit=7")
             .then(res => res.json())
             .then(data => setStationboard(data.stationboard || []))
             .catch(error => console.error('Error fetching connections:', error));
-    }, []);
-
-    // Fetch für die Leaderboard-Daten
-    useEffect(() => {
-        fetch("http://192.168.100.16:8080/api/trainees/leaderboard", {
-            headers: {
-                'Authorization': `Bearer ${apiToken}`,
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(res => res.json())
-            .then(data => {
-
-                const sortedLeaderboard = data.sort((a, b) => b.points - a.points);
-                setLeaderboard(sortedLeaderboard);
-            })
-            .catch(error => console.error('Error fetching leaderboard:', error));
-    }, [apiToken]);
-
-
-    const updateToken = () => {
-        fetch(AUTH_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: 'leaderboard',
-                password: 'leaderboard'
-            })
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data.token) {
-                    setApiToken(data.token);
-                } else {
-                    console.error('Error fetching new token');
-                }
-            })
-            .catch(error => console.error('Error updating token:', error));
-    };
-
-    useEffect(() => {
-
-        updateToken();
-
-
-        const intervalId = setInterval(updateToken, TOKEN_REFRESH_INTERVAL);
-
-
-        return () => clearInterval(intervalId);
-    }, []);
-
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setShowHelloWorld(prev => !prev);
-        }, 15000);
-
-
-        return () => clearInterval(intervalId);
     }, []);
 
     const roundToNextMinute = (date) => {
@@ -96,69 +28,41 @@ export default function SbbApi() {
         return `${hours}:${minutes}`;
     };
 
-    const renderStationboardCard = () => (
-        <div
-            className={styles.stationContainer}
-            onClick={() => setShowHelloWorld(true)}
-            style={{ cursor: 'pointer' }}
-        >
-            <h2 className={styles.cardTitle}>SBB Fahrplan</h2>
-            <table className={styles.styledTable}>
-                <thead>
-                <tr>
-                    <th>Von</th>
-                    <th>Abfahrtszeit</th>
-                    <th>Gleis</th>
-                    <th>Nach</th>
-                    <th>Ankunftszeit</th>
-                    <th>Name</th>
-                </tr>
-                </thead>
-                <tbody>
-                {stationboard.map((item, idx) => {
-                    const arrivalTime = item.passList.find(
-                        p => p.station.name === item.to
-                    )?.arrival;
-                    const platform = item.stop.platform.replace(/!/g, '');
-
-                    return (
-                        <tr key={idx}>
-                            <td>{item.stop.station.name}</td>
-                            <td>{formatTimeWithoutSeconds(roundToNextMinute(new Date(item.stop.departure)))}</td>
-                            <td>{platform}</td>
-                            <td>{item.to}</td>
-                            <td>{arrivalTime ? formatTimeWithoutSeconds(roundToNextMinute(new Date(arrivalTime))) : 'N/A'}</td>
-                            <td>{item.category + item.number}</td>
-                        </tr>
-                    );
-                })}
-                </tbody>
-            </table>
-        </div>
-    );
-
-    const renderLeaderboardCard = () => {
-        return (
-            <div
-                className={styles.helloWorldContainer}
-                onClick={() => setShowHelloWorld(false)}
-            >
-                <h2 className={styles.cardTitleHelloWorld}>AXA Coins Leaderboard</h2>
-                <ul className={styles.rankingsList}>
-                    {leaderboard.map((item, idx) => (
-                        <li key={idx} className={styles.rankingsItem}>
-                            {idx + 1}. {item.username} - {item.points.toFixed(0)} Coins
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
-    };
-
     return (
         <div className={styles.connectionInput}>
+            <h2 className={styles.cardTitle}>SBB Fahrplan</h2>
             <div className={styles.container}>
-                {showHelloWorld ? renderLeaderboardCard() : renderStationboardCard()}
+                <table className={styles["styled-table"]}>
+                    <thead>
+                    <tr>
+                        <th>Von</th>
+                        <th>Abfahrtszeit</th>
+                        <th>Gleis</th>
+                        <th>Nach</th>
+                        <th>Ankunftszeit</th>
+                        <th>Name</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {stationboard.map((item, idx) => {
+                        const arrivalTime = item.passList.find(
+                            p => p.station.name === item.to
+                        )?.arrival;
+                        const platform = item.stop.platform.replace(/!/g, '');
+
+                        return (
+                            <tr key={idx}>
+                                <td>{item.stop.station.name}</td>
+                                <td>{formatTimeWithoutSeconds(roundToNextMinute(new Date(item.stop.departure)))}</td>
+                                <td>{platform}</td>
+                                <td>{item.to}</td>
+                                <td>{arrivalTime ? formatTimeWithoutSeconds(roundToNextMinute(new Date(arrivalTime))) : 'N/A'}</td>
+                                <td>{item.category + item.number}</td>
+                            </tr>
+                        );
+                    })}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
